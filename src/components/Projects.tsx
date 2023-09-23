f3e60ddf060a21a72cbe81f266a42aa73aea3674
Eduardo Pereira Lima <140276300+Eduardopereira11967484018@users.noreply.github.com>
2023-09-23 15:25:45 -0300

    Adicionei arquivos ao repositório

---
+++ src/components/Projects.tsx
@@ -0,0 +1,93 @@
+import { HiArrowTopRightOnSquare } from "react-icons/hi2";
+
+export default function Projects() {
+  const projects = [
+    {
+      title: "Cordel Morderno",
+      description: "Neste projeto de desenvolvimento web, nosso foco principal é o aprendizado avançado de CSS, onde os participantes terão a oportunidade de aprofundar seus conhecimentos e habilidades em design e estilização de páginas da web. Este projeto se destina a aspirantes a desenvolvedores web, designers ou qualquer pessoa interessada em elevar suas habilidades em CSS a um nível mais avançado.",
+      image:
+        "https://images.unsplash.com/photo-1487014679447-9f8336841d58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1605&q=80",
+      link: "https://eduardopereira11967484018.github.io/projcordel/",
+      githubLink: "https://github.com/seuusuario/seuprojeto1",
+      colSpan: "col-span-1",
+    },
+    {
+      title: "portifolio",
+      description: "Meu portfólio, criado com HTML, CSS e JavaScript no início do meu aprendizado, reflete minha jornada de crescimento e paixão pelo desenvolvimento web, mostrando como tenho progredido na arte de criar websites incríveis.",
+      image:
+        "https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHdlYnNpdGVzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+      link: " https://eduardopereira11967484018.github.io/portfolio01/",
+      colSpan: "col-span-1 md:col-span-2",
+    },
+    {
+      title: "Tela de login",
+      description: "Minha tela de login, fruto do início da minha jornada de aprendizado, é um exemplo do meu comprometimento em aprimorar minhas habilidades de responsividade. Ela reflete como estou explorando as nuances do design adaptativo para criar interfaces atraentes em todos os dispositivos.",
+      image:
+        "https://media.istockphoto.com/id/1305995602/photo/responsive-floating-responsive-design.jpg?b=1&s=170667a&w=0&k=20&c=uFqBz27v_B0UxcslnvZxd9c0D9eJAef7veRZf6b-v-A=",
+      link: "https://eduardopereira11967484018.github.io/projlogin/",
+      colSpan: "col-span-1",
+    },
+    {
+      title: "Calculadora",
+      description: "Minha calculadora é um projeto humilde, mas representativo, que marca o início da minha jornada de aprendizado em JavaScript. Com ele, estou construindo uma base sólida nos conceitos essenciais da linguagem, enquanto descubro como transformar ideias em funcionalidades interativas.",
+      image:
+        "https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y29kaW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+      link: "https://eduardopereira11967484018.github.io/calculadora/",
+      colSpan: "col-span-1",
+    },
+    {
+      title: "Quiz",
+      description: "Meu projeto de quiz, desenvolvido no início da minha jornada de aprendizado em JavaScript, exemplifica minha dedicação em compreender os conceitos de verdadeiro e falso (true e false) na programação. É um passo inicial em direção à construção de experiências interativas e educativas",
+      image:
+        "https://images.unsplash.com/photo-1551650975-87deedd944c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bW9iaWxlJTIwYXBwfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+      link: "https://eduardopereira11967484018.github.io/Quiz-interativo/",
+      colSpan: "col-span-1",
+    },
+  
+    // Outros projetos aqui...
+  ];
+
+  return (
+    <>
+      <section className="rounded-tl-[80px] bg-gradient-to-tr from-black to-gray-900 text-white md:rounded-tl-[180px]">
+        <div className="container mx-auto max-w-4xl p-4 py-12">
+          <div className="relative p-4 text-center">
+            <h2 className="relative z-50 mb-2 text-white">
+              <span className="mr-2 font-headline text-3xl font-semibold">
+                Projetos &
+              </span>
+              <span className="font-handwriting text-4xl">Portfólio</span>
+            </h2>
+            <p className="relative text-sm text-gray-400">
+              Alguns dos projetos pessoais e que já realizei ao longo da minha
+              trajetória como programador front-end.
+            </p>
+            <div className="absolute left-1/2 top-3 z-0 h-10 w-10 rounded-lg bg-blue-400/10" />
+          </div>
+
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+            {projects.map((project, index) => (
+              <a
+                key={index}
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`group relative h-52 cursor-pointer rounded-lg ${project.colSpan} bg-cover bg-center`}
+                style={{ backgroundImage: `url('${project.image}')` }}
+              >
+                <div className="absolute inset-0 flex h-full w-full flex-col items-center justify-center rounded-lg bg-blue-600 text-white opacity-0 transition-opacity group-hover:opacity-100">
+                  <h4 className="font-headline text-lg font-semibold">
+                    {project.title}
+                  </h4>
+                  <p className="mb-4 text-sm">{project.description}</p>
+                  <HiArrowTopRightOnSquare className="h-6 w-6" />
+                </div>
+              </a>
+            ))}
+          </div>
+        </div>
+      </section>
+      <div className="absolute right-0 -mt-[6px] h-3 w-48 rounded-l-full bg-gradient-to-r from-gray-700 to-gray-600 md:w-96" />
+    </>
+  );
+}
